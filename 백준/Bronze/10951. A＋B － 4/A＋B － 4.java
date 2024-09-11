a6b5df5f93853591dfb4efbb08154d8fb47565e3
rkddkdus8595 <76616541+rkddkdus8595@users.noreply.github.com>
2024-09-11 17:11:15 +0900

    [Bronze V] Title: A+B - 4, Time: 184 ms, Memory: 17932 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        int b,c;
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNextInt()){
+            b = sc.nextInt();
+            c = sc.nextInt(); 
+            System.out.println(b+c);        
+        }
+	}
+}
