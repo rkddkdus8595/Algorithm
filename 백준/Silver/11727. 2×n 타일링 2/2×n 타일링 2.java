1c763e446d4f300d17c640b7ec27687067578005
rkddkdus8595 <76616541+rkddkdus8595@users.noreply.github.com>
2024-11-01 17:14:21 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 184 ms, Memory: 17852 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main {
+    static Integer[] dp;	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        dp = new Integer[n+1];
+        dp[0] = dp[1] = 1; 
+        System.out.print(recur(n));
+	}
+	static int recur(int n) {
+		if(dp[n] == null) {
+			dp[n] = (recur(n-1)+recur(n-2)*2)%10007;
+		}
+		return dp[n];
+	}
+}
