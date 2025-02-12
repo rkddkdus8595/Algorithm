8916910b9fd521c5d2eec3faf71f96e9968fd4f2
rkddkdus8595 <76616541+rkddkdus8595@users.noreply.github.com>
2025-02-12 15:26:48 +0900

    [Bronze V] Title: 아스키 코드, Time: 164 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		char num = sc.next().charAt(0);
+		System.out.println((int)num);
+    }
+}
